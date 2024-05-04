a2ed8c1a32f2cf5e6759365801e0a1b541649c24
Gustavo Lopes <gusflopes86@gmail.com>
2024-05-04 19:40:31 -0400

    chore: reestruturando projeto

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 dotnet new webapi --name YarpDevInfra.GatewayAPI --output ./src/YarpDevInfra.GatewayAPI
 dotnet new webapi --name YarpDevInfra.SampleAPI --output ./src/backend/YarpDevInfra.SampleAPI
 ```
-
+docker run -d --name sample-api2 -p 5001:8080  gusflopes/yarp-sample
 
 dotnet sln add ./src/YarpDevInfra.GatewayAPI/YarpDevInfra.GatewayAPI.csproj
--- src/YarpDevInfra.GatewayAPI/Program.cs
+++ src/YarpDevInfra.GatewayAPI/Program.cs
@@ -8,21 +8,11 @@ using Yarp.ReverseProxy.Model;
 var routes = new List<RouteConfig>();
 var clusters = new List<ClusterConfig>();
 
-var sampleRouteConfig = new RouteConfig()
+var defaultRouteConfig = new RouteConfig()
 {
     RouteId = "default",
     ClusterId = "default",
     Match = new RouteMatch() { Path = "{**catch-all}" }
-};
-
-var sampleClusterConfig = new ClusterConfig()
-{
-    ClusterId = "sample-1",
-    Destinations = new Dictionary<string, DestinationConfig>()
-    {
-        { "default", new DestinationConfig() { Address = "http://localhost:5001" } },
-        // { "destination3", new DestinationConfig() { Address = "http://localhost:5002" } }
-    }
 };
 
 var defaultCluster = new ClusterConfig()
@@ -33,19 +23,8 @@ var defaultCluster = new ClusterConfig()
         { "default", new DestinationConfig() { Address = "http://localhost:5000" } }
     }
 };
-//
-// var sample2ClusterConfig = new ClusterConfig()
-// {
-//     ClusterId = "sample-2",
-//     Destinations = new Dictionary<string, DestinationConfig>()
-//     {
-//         { "default", new DestinationConfig() { Address = "http://localhost:5002" } }
-//     }
-// };
 
-routes.Add(sampleRouteConfig);
-clusters.Add(sampleClusterConfig);
-// clusters.Add(sample1ClusterConfig);
+routes.Add(defaultRouteConfig);
 clusters.Add(defaultCluster);
 
 var builder = WebApplication.CreateBuilder(args);
@@ -82,7 +61,7 @@ app.MapReverseProxy(proxyPipeline =>
         var host = context.Request.Host;
         if (headers != null)
         {
-            Console.WriteLine("Do something here!");
+            Console.WriteLine($"Recebido headers X-Test-Version: {headers}");
         }
 
         if (lookup.TryGetCluster(ChooseCluster(context), out var cluster))
@@ -156,112 +135,6 @@ app.MapPost("/admin/clusters", (AddClusterRequest requestBody, InMemoryConfigPro
     return Results.Ok();
 });
 
-// app.MapPost("/admin/destinations", (AddDestinationRequest requestBody, InMemoryConfigProvider configProvider) =>
-// {
-//     if (requestBody != null)
-//     {
-//         var config = configProvider.GetConfig();
-//         var clusters = config.Clusters.ToList();
-//         // var cluster = clusters.FirstOrDefault(x => x.ClusterId == "SampleCluster");
-//         var cluster = clusters.FirstOrDefault();
-//         if (cluster != null)
-//         {
-//             var newDestinations = cluster.Destinations.ToDictionary();
-//             newDestinations.Add("destination2", new DestinationConfig() { Address = "http://localhost:5001"});
-//
-//             var updatedCluster = new ClusterConfig()
-//             {
-//                 ClusterId = cluster.ClusterId,
-//                 Destinations = newDestinations
-//             };
-//
-//             var index = clusters.FindIndex(x => x.ClusterId == cluster.ClusterId);
-//             clusters[index] = updatedCluster;
-//
-//             configProvider.Update(config.Routes, clusters);
-//         }
-//     }
-//     else
-//     {
-//         return Results.BadRequest("Invalid request body");
-//     }
-//
-//     return Results.Ok();
-//
-// });
-//
-// app.MapGet("admin/destinations", (InMemoryConfigProvider configProvider) =>
-// {
-//     var config = configProvider.GetConfig();
-//     var destinations = config
-//         .Clusters.
-//         FirstOrDefault()
-//         .Destinations
-//         .ToArray();
-//     return Results.Ok(destinations);
-// });
-//
-
-
-
-
-
-
-// app.MapDelete("admin/destinations/{destinationKey}", (string destinationKey, InMemoryConfigProvider configProvider) =>
-// {
-//
-//     var config = configProvider.GetConfig();
-//     var clusters = config.Clusters.ToList();
-//     var cluster = clusters.FirstOrDefault();
-//
-//     if (cluster != null)
-//     {
-//         var destinations = cluster.Destinations.ToDictionary();
-//         if (destinations.ContainsKey(destinationKey))
-//         {
-//             destinations.Remove(destinationKey);
-//
-//             var updatedCluster = new ClusterConfig()
-//             {
-//                 ClusterId = cluster.ClusterId,
-//                 Destinations = destinations
-//             };
-//             var index = clusters.FindIndex(x => x.ClusterId == cluster.ClusterId);
-//             clusters[index] = updatedCluster;
-//
-//             configProvider.Update(config.Routes, clusters);
-//
-//             return Results.Ok($"Destination with key '{destinationKey}' has been deleted.");
-//         }
-//         else
-//         {
-//             return Results.BadRequest($"Destination with key '{destinationKey}' not found.");
-//         }
-//     }
-//     else
-//     {
-//         return Results.NotFound("Cluster not found.");
-//     }
-// });
-
-// app.MapGet("admin/destinations", (InMemoryConfigProvider configProvider) =>
-// {
-//     var config = configProvider.GetConfig();
-//     var destinations = config
-//         .Clusters.
-//         FirstOrDefault()
-//         .Destinations
-//         .ToArray();
-//
-//     return Results.Ok(destinations);
-//
-// });
-
 app.Run();
 
-
-
-public record AddDestinationRequest(string Key, string hostAddress);
-
 public record AddClusterRequest(string appName, int versionNumber, string hostAddress);
-
