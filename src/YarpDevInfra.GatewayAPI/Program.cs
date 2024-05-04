033ee4c0f3241b9c86df1a8ded47cb6ac6d58074
Gustavo Lopes <gusflopes86@gmail.com>
2024-05-04 13:28:14 -0400

    feat: yarp implementation

--- src/YarpDevInfra.GatewayAPI/Program.cs
+++ src/YarpDevInfra.GatewayAPI/Program.cs
@@ -1,47 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<IdentifierService>();
+builder.Services.AddReverseProxy()
+.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-
-app.MapGet("/sample", (IdentifierService service) =>
-{
-    var response = new
-    {
-        Identifier = service.GetIdentifier(),
-        Message = "Hello from this Sample API"
-    };
-    return response;
-})
-.WithName("Sample")
-.WithOpenApi();
-
+app.MapReverseProxy();
 app.Run();
-
-public class IdentifierService
-{
-    private readonly string _identifier;
-    public IdentifierService()
-    {
-        _identifier = new Random().Next().ToString();
-    }
-    public string GetIdentifier()
-    {
-        return _identifier;
-    }
-}
