09869b1c84642dddec5056a34d6df8ac79c60233
Gustavo Lopes <gusflopes86@gmail.com>
2024-05-05 10:25:05 -0400

    feat: adicionado CORS na sample API

--- src/YarpDevInfra.SampleAPI/Program.cs
+++ src/YarpDevInfra.SampleAPI/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    });
+});
 
 builder.Services.AddSingleton<IdentifierService>();
 
@@ -18,30 +30,52 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapPost("/sample", async (HttpContext context, IdentifierService service) =>
+{
+
+    var newRecord = await context.Request.ReadFromJsonAsync<IdentifierService.CreateSampleData>();
+    if (newRecord is null)
+    {
+        return Results.BadRequest();
+    }
+    service.CreateData(newRecord);
+    return Results.Created();
+});
+
 
 app.MapGet("/sample", (IdentifierService service) =>
 {
     var response = new
     {
-        Identifier = service.GetIdentifier(),
-        Message = "Hello from this Sample API"
+        appId = service.Identifier,
+        records = service.Data
     };
     return response;
 })
 .WithName("Sample")
 .WithOpenApi();
 
+// Preciso habilitar CORS
+app.UseCors("AllowAllOrigins");
+
 app.Run();
 
 public class IdentifierService
 {
+    public record SampleData(Guid Id, string Description);
+    public record CreateSampleData(string Description);
+
     private readonly string _identifier;
+    private List<SampleData> _data { get; set; } = new();
     public IdentifierService()
     {
         _identifier = new Random().Next().ToString();
+        _data.Add(new SampleData(Guid.NewGuid(), "Primeiro Registro."));
     }
-    public string GetIdentifier()
+    public string Identifier { get => _identifier; }
+    public IEnumerable<SampleData> Data { get => _data.ToArray(); }
+    public void CreateData(CreateSampleData data)
     {
-        return _identifier;
+        _data.Add(new SampleData(Guid.NewGuid(), data.Description));
     }
 }
